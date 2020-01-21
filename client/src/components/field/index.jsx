6c0198e9f2babfbc6f006342fc366c9f2f878a80
anna-kuzii <anna_kuzij@ukr.net>
2020-01-22 00:35:05 +0200

    add validation

--- client/src/components/field/index.jsx
+++ client/src/components/field/index.jsx
@@ -2,19 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './style.scss';
 
-export const FormField = ({ label, type, name, placeholder, onChangeInput, value }) => {
+export const FormField = ({ label, type, name, placeholder, onChangeInput, value, error }) => {
   const handleChange = (e) => {
-    onChangeInput({
-      ...value,
-      [name]: e.target.value,
-    });
+    onChangeInput(name, e.target.value);
   };
 
   return (
     <fieldset className="field-form">
       <label>{label}
-        <input type={type} placeholder={placeholder} onChange={handleChange} value={value[name]}/>
+        <input type={type} placeholder={placeholder} onChange={handleChange} value={value[name]} />
       </label>
+      {error && <p className="field-error">{error}</p>}
     </fieldset>
   );
 };
@@ -24,6 +22,7 @@ FormField.propTypes = {
   type: PropTypes.string,
   placeholder: PropTypes.string,
   name: PropTypes.string,
-  value: PropTypes.object,
+  value: PropTypes.string,
   onChangeInput: PropTypes.func,
+  error: PropTypes.string,
 };
--- client/src/containers/RegisterForm/index.jsx
+++ client/src/containers/RegisterForm/index.jsx
@@ -3,16 +3,51 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { FormField } from '../../components/field';
 import { submitRegister } from './actions';
+import { errors, inputRules } from '../../constants/formValidation';
 import './style.scss';
 
 const RegisterForm = ({ submitRegister }) => {
-  const [formData, onSubmitReg] = useState({
+  const formFiled = {
     name: '',
     surname: '',
     username: '',
     email: '',
     password: '',
-  });
+  };
+
+  const [formData, onSubmitReg] = useState(formFiled);
+
+  const [error, setError] = useState(formFiled);
+
+  const validateField = (name, value) => {
+    const rule = new RegExp(inputRules[name]);
+
+    return rule.test(value) ? '' : errors[name];
+  };
+
+  const onHandleChange = (name, value) => {
+    const errMsg = validateField(name, value);
+
+    if (errMsg) {
+      setError({
+        ...error,
+        [name]: errMsg,
+      });
+    } else {
+      setError({
+        ...error,
+        [name]: '',
+      });
+      onSubmitReg({
+        ...formData,
+        [name]: value,
+      });
+    }
+  };
+
+  const checkValidation = () => {
+    return Object.keys(error).every((k) => !error[k]) && Object.values(formData).every(Boolean);
+  };
 
   const onHandleRegistration = (e) => {
     e.preventDefault();
@@ -27,46 +62,52 @@ const RegisterForm = ({ submitRegister }) => {
         name="name"
         type="text"
         placeholder="John"
-        value={formData}
-        onChangeInput={onSubmitReg}
+        value={formData.name}
+        onChangeInput={onHandleChange}
+        error={error.name}
       />
       <FormField
         label="Surname"
         name="surname"
         type="text"
         placeholder="Doe"
-        value={formData}
-        onChangeInput={onSubmitReg}
+        value={formData.surname}
+        onChangeInput={onHandleChange}
+        error={error.surname}
       />
       <FormField
         label="Username"
         name="username"
         type="text"
         placeholder="JDoe"
-        value={formData}
-        onChangeInput={onSubmitReg}
+        value={formData.username}
+        onChangeInput={onHandleChange}
+        error={error.username}
       />
       <FormField
         label="Email"
         name="email"
         type="email"
         placeholder="jonhdoe@gmail.com"
-        value={formData}
-        onChangeInput={onSubmitReg}
+        value={formData.email}
+        onChangeInput={onHandleChange}
+        error={error.email}
       />
       <FormField
         label="Password"
         name="password"
         type="password"
         placeholder="4 + characters"
-        value={formData}
-        onChangeInput={onSubmitReg}
+        value={formData.password}
+        onChangeInput={onHandleChange}
+        error={error.password}
       />
 
       <button
         type="submit"
         className="sign-up"
         onClick={(e) => onHandleRegistration(e)}
+        disabled={!checkValidation()}
       >Sign Up</button>
     </form>
   );
