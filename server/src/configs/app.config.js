bc20446474e89e09bd241d38287ad5275182ae18
anna-kuzii <anna_kuzij@ukr.net>
2020-01-22 14:35:36 +0200

    add email

--- server/src/configs/app.config.js
+++ server/src/configs/app.config.js
@@ -7,4 +7,6 @@ export default {
   DATABASE_URL: process.env.DATABASE_URL,
   CLIENT_PATH: 'client/build',
   ALLOWED_ORIGINS: ['http://127.0.0.1:3000', 'http://localhost:3000'],
+  EMAIL: process.env.EMAIL || 'girls.sport.news',
+  PASSPORT: process.env.PASSPORT || 'SportNews@)19',
 };
